233f9496c7857f1c4654cd6ab3318260d7525196
Ashutosh <ashutoshmark18@gmail.com>
2021-09-28 16:07:00 +0530

    Folder Struct Setup: Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+// import { MenuService } from './menu.service';
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+
+
+interface MenuNode {
+  name: string;
+  mId: string;
+  childs?: MenuNode[];
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,95 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'EXHCHANGE-PORTAL-UI';
+
+  treeControl = new NestedTreeControl<MenuNode>(node => node.childs); 
+  dataSource = new MatTreeNestedDataSource<MenuNode>();
+  mainMenuData:any = []  
+  allMenuData:any = [] 
+  uiToogler = {
+    menuSelected: "",
+    button : "Create"
+  }
+  
+  menuData = {
+    mid: "",
+    name: "",
+    description: "",
+    route: "",
+    permission: "",
+    parent_id: "",
+    projId : "P_001"
+  }
+  constructor() { 
+      this.refreshMenu();
+   }
+
+   hasChild = (_: number, node: MenuNode) => !!node.childs && node.childs.length > 0; // !! = ?
+   onClick(menuId: any){
+     this.uiToogler.button = "Update"
+    for (const menu of this.allMenuData) {
+      if(menu.mId === menuId){
+        this.menuData = menu
+        this.uiToogler.menuSelected = menuId
+      }
+    }
+    event?.stopPropagation()
+  }
+
+
+  refreshMenu(){
+    // this.menuService.getMainMenuData(this.menuData.projId).subscribe(data =>{
+    //   this.mainMenuData = data
+    // })
+    // this.menuService.getAllMenuData(this.menuData.projId).subscribe(data =>{
+        //   this.allMenuData = data
+        // })
+        
+        this.mainMenuData = [
+            {
+          "mId":"M_001",
+          "pid":"P_001",
+          "name":"Home",
+          "desc":"home",
+          "route":"/home",
+          "permission":"HOME",
+          "childs":[]
+        },
+        {
+          "mId":"M_002",
+          "name":"Petstore",
+          "desc":"Petstore",
+          "route":"/petstore",
+          "permission":"SALE_TRANSACTION",
+          "childs":[
+              {
+                  "mId":"M_003",
+                  "name":"petstore.json",
+                  "desc":"petstore.json",
+                  "route":"/petstore.json",
+                  "permission":"PETSTORE_JSON",
+                }
+            ]
+        },
+        {
+            "mId":"M_002",
+            "name":"AppUi",
+            "desc":"appui",
+            "route":"/app-ui",
+            "permission":"APP_UI",
+            "childs":[
+                {
+                    "mId":"M_003",
+                    "name":"appui.json",
+                    "desc":"appui.json",
+                    "route":"/appui.json",
+                    "permission":"APP_UI_JSON",
+                  }
+              ]
+          }
+    ]
+    this.dataSource.data = this.mainMenuData;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import {HttpClientModule } from '@angular/common/http';
+import { DemoMaterialModule } from './material-module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
