9378fcd24f9969aa86cc34dfbc9307c667e59731
joynath76 <jborman76@gmail.com>
2021-09-29 17:50:42 +0530

    Integrated file with swaggerUI

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,8 +10,15 @@ export class AppService {
     constructor(private http: HttpClient) { }
 
     getAllMenu(){ // we will use this
-        let api_url = "http://www.localhost:8080/";
+        let api_url = "http://localhost:8080/exchange-portal-0.0.1-SNAPSHOT/";
         return this.http.get(api_url);
+    }
+
+    getContentOfFile(data:any){
+      let api_url = "http://localhost:8080/exchange-portal-0.0.1-SNAPSHOT/";
+      const headers = { 'content-type': 'application/json'}
+      const body=JSON.stringify(data);
+      return this.http.post(api_url, body,{'headers':headers})
     }
 
 }
--- src/app/swagger-ui/swagger-ui.component.ts
+++ src/app/swagger-ui/swagger-ui.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AppService } from '../app.service';
-import {NestedTreeControl} from '@angular/cdk/tree';
-import {MatTreeNestedDataSource} from '@angular/material/tree';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 declare const SwaggerUIBundle: any;
 interface MenuNode {
   name: string;
-  files?: string;
   childs?: MenuNode[];
 }
 @Component({
@@ -17,104 +16,53 @@ export class SwaggerUiComponent implements OnInit {
 
   title = 'EXCHANGE-PORTAL-UI';
 
-  treeControl = new NestedTreeControl<MenuNode>(node => node.childs); 
+  treeControl = new NestedTreeControl<MenuNode>(node => node.childs);
   dataSource = new MatTreeNestedDataSource<MenuNode>();
-  mainMenuData:any = []  
-  allMenuData:any = [] 
-  uiToogler = {
-    menuSelected: "",
-    button : "Create"
-  }
+  mainMenuData: any = []
 
-  ngOnInit(): void {
-    const ui = SwaggerUIBundle({
-        dom_id: '#swagger-ui',
-        layout: 'BaseLayout',
-        presets: [
-          SwaggerUIBundle.presets.apis,
-          SwaggerUIBundle.SwaggerUIStandalonePreset
-        ],
-        url: 'https://petstore.swagger.io/v2/swagger.json',
-      });
-  }
-
-  constructor(private appService: AppService) { 
+  editor : any
+  constructor(private appService: AppService) {
     this.refreshMenu();
- }
+  }
+  ngOnInit(): void {
+    this.editor = SwaggerUIBundle({
+      dom_id: '#swagger-ui',
+      layout: 'BaseLayout',
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
+      ],
+      url: 'https://petstore.swagger.io/v2/swagger.json',
+    });
+  }
 
- hasChild = (_: number, node: MenuNode) => !!node.childs && node.childs.length > 0; // !! = ?
-   onClick(nameSearch: any){
-     this.uiToogler.button = "Update"
-    for (const menu of this.allMenuData) {
-      if(menu.name === nameSearch){
-        // this.menuData = menu
-        this.uiToogler.menuSelected = nameSearch;
-      }
+  hasChild = (_: number, node: MenuNode) => !!node.childs && node.childs.length > 0; // !! = ?
+
+
+  fileContent:any = []
+  async loadEditorSpec(node: any){
+    this.appService.getContentOfFile(node).subscribe(data =>{
+      console.log(data)
+      this.fileContent = data;
+      let specDetails = "";
+      if(data != null) specDetails = this.fileContent["content"]
+      this.editor.specActions.updateSpec(specDetails)
+    })
+  }
+
+  async onClick(node: any){
+    if(node.type != 1){
+      await this.loadEditorSpec(node)
     }
     event?.stopPropagation()
-  }
+ }
 
 
-  refreshMenu(){
-    
-    this.appService.getAllMenu().subscribe(data =>{
-        this.mainMenuData = data
-        this.dataSource.data = this.mainMenuData;
+  refreshMenu() {
+    this.appService.getAllMenu().subscribe(data => {
+      this.mainMenuData = data
+      this.dataSource.data = this.mainMenuData;
     })
-
-
-    // this.mainMenuData = [
-    //     {
-    //         "name": "Music",
-    //         // "route": "/{name}",
-    //         "files": [
-    //         "music.json"
-    //         ],
-    //         "childs": [
-    //         {
-    //             "name": "New folder",
-    //             "files": [],
-    //             "childs": [
-    //             {
-    //                 "name": "nested",
-    //                 "files": [],
-    //                 "childs": []
-    //             }
-    //             ]
-    //         },
-    //         {
-    //             "name": "Sub Music",
-    //             "files": [
-    //             "new folder.json"
-    //             ],
-    //             "childs": []
-    //         }
-    //         ]
-    //     },
-    //     {
-    //         "name": "New User",
-    //         "files": [
-    //         "new user.json"
-    //         ],
-    //         "childs": []
-    //     },
-    //     {
-    //         "name": "Order",
-    //         "files": [
-    //         "order.json"
-    //         ],
-    //         "childs": []
-    //     },
-    //     {
-    //         "name": "Service",
-    //         "files": [
-    //         "service.json"
-    //         ],
-    //         "childs": []
-    //     }
-    // ]
-    
-
   }
 
 }
