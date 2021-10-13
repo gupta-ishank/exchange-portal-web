4126b425404fe9d9d85ec9992f94265602dffcd5
gupta-ishank <ishank.gupta@netmeds.com>
2021-10-13 17:00:17 +0530

    fixed tables bugs

--- src/app/custom-component/custom-component.component.ts
+++ src/app/custom-component/custom-component.component.ts
@@ -84,6 +84,16 @@ export class CustomComponentComponent implements OnInit{
         name: 'Type',
         header: 'Type',
         width: '200px'
+      },
+      {
+        name: 'Mendate',
+        header: 'Mandatory',
+        width: '200px'
+      },
+      {
+        name: 'Description',
+        header: 'Description',
+        width: '200px'
       }
     ],
     subgrid_config: {
